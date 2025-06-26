774b319cd9450bb8b76697e27bc580cea101812f
Sleax-hub <sleaxacademy@gmail.com>
2025-06-26 17:59:13 +0300

    nav update

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Icon from "react-native-feather"
+
 
 export default function Index() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 1
       }}
+      className="bg-black"
     >
-      <Text className="text-xl font-bold text-red-500">Edit app/Hello youtube</Text>
+      <SafeAreaView className="flex-row justify-between mx-4 mt-2">
+        <View className="flex-row items-center gap-x-1">
+          <Image source={require('../assets/images/youtlogo.png')} className='h-7 w-10 rounded-xl' />
+          <Text className="text-white font-semibold text-xl tracking-tightier">YouTube</Text>
+        </View>
+
+        <View className="flex-row items-center gap-x-3">
+          <Icon.Cast stroke="white" strokeWidth="1.2" height="22"/>
+          <Icon.Bell stroke="white" strokeWidth="1.2" height="22"/>
+          <Icon.Search stroke="white" strokeWidth="1.2" height="22"/>
+          <Image source={require("../assets/images/face.jpg")} className="h-7 w-7 rounded-full" />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
